0b80cc8a0900f5ea2b528eb213a5ede893e5d59d
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-06-21 19:12:41 +0200

    Update main.c

--- main.c
+++ main.c
@@ -8,10 +8,41 @@
  * main - point of entry for shell
  * @argc: argument count
  * @argv: argument vector
- * Return: no return value
+ * Return: 0 for failure
  */
 
 int main(int argc, char **argv)
 {
-	
+	const char *display = "simpleshell> ";
+	char *input;
+	size_t len_display = sizeof(display), max_line_size = 1024;
+	int line_len;
+
+	if (argc > 1)
+	{
+		exit(0);
+	}
+
+	for (;;)
+	{
+		input = malloc(200);
+		write(0, display, len_display);
+		line_len = getline(&input, &max_line_size, stdin);
+		if (line_len == -1)
+		{
+			free(input);
+			return (0);
+		}
+		else if (line_len > 1)
+		{
+			input[line_len - 1} = '\0';
+			if (input)
+			{
+				/*add parse function
+				*/
+			}
+			free(input);
+		}
+	}
+		
 }
