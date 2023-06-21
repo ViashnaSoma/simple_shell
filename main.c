07b61226a871f694e657b12ad3e9da266421e789
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-06-21 18:05:46 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <sys/wait.h>
+#include "shell.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - point of entry for shell
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: no return value
+ */
+
+int main(int argc, char **argv)
+{
+	
+}
