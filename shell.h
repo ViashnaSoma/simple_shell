80bebd5fb319d9afb946711242aa8266e599a981
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-06-21 18:18:05 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -8,4 +8,23 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+extern char **envir;
+
+/**
+ * struct command - stores all arguments passed to function
+ * command: command passed to function
+ * @envrn: array containing pointers pointing to environ variables
+ * @normal: original
+ * @argc: argument count
+ * @argv: array of arguments
+ * Description: Command structure
+ */
+typedef struct command
+{
+	int argc;
+	char *argv[128];
+	char **envrn;
+	char *normal;
+} command;
+
 #endif
