0c675263edb4aa88aedf9d04ba14c62da2b05095
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-06-21 19:20:11 +0200

    Create parsing.c

---
+++ parsing.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * parsing - separates words from input based on
+ * spaces and sends for evaluation
+ * @input: string that needs to be tokenised
+ * @envrn: the global environment
+ * @normal: name of executable file
+ * Return: nothing
+ */
+
+void parsing(char *input, char **envrn, char *normal)
+{
+}
