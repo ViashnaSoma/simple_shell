8e3fdc87c1303c661c7cf38eb0e704f34044f299
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-06-24 11:45:59 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -2,11 +2,11 @@
 #define SIMPLE_SHELL
 #define ARG_LIMIT 128
 
+#include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/stat.h>
 
 extern char **environ;
