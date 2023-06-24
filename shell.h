6ab6713ad2dd293277e4d72f1357641aebe1ee63
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-06-24 11:43:58 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,6 +1,6 @@
 #ifndef SIMPLE_SHELL
 #define SIMPLE_SHELL
-#define ARGS_LIMIT 128
+#define MAXARGS 128
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,9 +23,9 @@ extern char **environ;
 typedef struct cmd
 {
 	int argc;
-	char *home;
+	char *argv[MAXARGS];
 	char **env;
-	char *argv[ARGS_LIMIT];
+	char *home;
 } cmd;
 
 /**
@@ -36,24 +36,24 @@ typedef struct cmd
  */
 typedef struct builtins
 {
-	void (*func)(cmd *);
 	char *instruction;
+	void (*func)(cmd *);
 } builtins_table;
 
-void my_exit(cmd *cmd_struct);
-void my_cd(cmd *cmd_struct);
 void parse(char *command, char **env, char *home);
 void execute(struct cmd *cmd_struct, char *new_path);
 void (*is_builtin(char *command))(cmd *cmd_struct);
-void eval(cmd *cmd_struct);
+void my_cd(cmd *cmd_struct);
+void my_exit(cmd *cmd_struct);
 void my_env(cmd *cmd_struct);
+void eval(cmd *cmd_struct);
 
-char *check_var(char *env_var, char *var);
-char *_strcat(char *dest, char *src);
-char *handle_path(cmd *cmd_struct, char *checkpath);
 int _strcmp(char *s1, char *s2);
 char *get_env_var(char **environ, char *var);
+char *check_var(char *env_var, char *var);
+char *handle_path(cmd *cmd_struct, char *checkpath);
+char *_strcat(char *dest, char *src);
 
-size_t get_len(char *s);
 int check_file(char *file);
+size_t get_len(char *s);
 #endif
