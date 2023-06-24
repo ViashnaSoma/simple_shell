c8ace75df1df15f040b024f16346900d3c28ffd3
ViashnaSoma <117302551+ViashnaSoma@users.noreply.github.com>
2023-06-24 10:25:33 +0200

    Update evaluation.c

--- evaluation.c
+++ evaluation.c
@@ -1,28 +1,25 @@
 #include "shell.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 
 /**
- * Implement your logic to check if the command is a built-in command
- * Implement your logic to execute the built-in command based on the provided 'command' structure
- * command not there show "invalid comman"
- *
- * Return 1 if it is a built-in command, 0 otherwise
+ * eval - Determines whether the command is builtin,
+ * system or invalid and takes action.
+ * @cmd_struct: command structure.
+ * Return: void.
  */
-void eval(command *command_struct)
+
+void eval(cmd *cmd_struct)
 {
 	char checkpath[20] = "/bin/", *new_path;
 
-	void (*func)(command *command_struct) = is_builtin(command_struct->argv[0]);
+	void (*func)(cmd *cmd_struct) = is_builtin(cmd_struct->argv[0]);
 
 	if (func)
 	{
-		func(command_struct);
+		func(cmd_struct);
 	}
 	else
 	{
-		new_path = handle_path(command_struct, checkpath);
-		exec(command_struct, new_path);
+		new_path = handle_path(cmd_struct, checkpath);
+		execute(cmd_struct, new_path);
 	}
 }
